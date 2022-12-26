65543a66fa2a41582ecc4b13897b8e75e8e3263b
Gabriel Cesarino <61214104+gabrielCesarino@users.noreply.github.com>
2022-12-26 11:56:27 -0300

    add: TouchableOpacity item to handle onPress function on categories

--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -3,35 +3,37 @@ import { FlatList } from "react-native"
 import { categories } from "../../mocks/categories"
 import { Text } from "../Text"
 import { Category, Icon } from "./styles"
+import { useState } from 'react';
 
 export function Categories() {
+
+	const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+	function handleSelectCategory(categoryId: string) {
+		const category = selectedCategory === categoryId ? '' : categoryId;
+		setSelectedCategory(category);
+	}
+
 	return (
-		<>
-			<FlatList
-				horizontal
-				showsHorizontalScrollIndicator= {false}
-				data={categories}
-				contentContainerStyle={{ paddingRight: 24}}
-				keyExtractor={category => category._id}
-				renderItem={({ item: category }) => (
-					<Category>
+		<FlatList
+			horizontal
+			showsHorizontalScrollIndicator={false}
+			data={categories}
+			contentContainerStyle={{ paddingRight: 24 }}
+			keyExtractor={category => category._id}
+			renderItem={({ item: category }) => {
+				const isSelected = selectedCategory === category._id;
+
+				return (
+					<Category onPress={() => handleSelectCategory(category._id)}>
 						<Icon>
-							<Text>{category.icon}</Text>
+							<Text opacity={isSelected ? 1 : 0.5 }>{category.icon}</Text>
 						</Icon>
 
-						<Text size={14} weight='600'>{category.name}</Text>
+						<Text size={14} weight='600' opacity={isSelected ? 1 : 0.5 }>{category.name}</Text>
 					</Category>
-				)}
-			/>
-			{/* categories.map((category) => (
-			<Category key={category._id}>
-				<Icon>
-					<Text>{category.icon}</Text>
-				</Icon>
+				)
+			}}
+		/>
 
-				<Text size={14} weight='600'>{category.name}</Text>
-			</Category>
-			)) */}
-		</>
 	)
 }
