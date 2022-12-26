aff9e4cefdc0c809300bd510ff31fc81b9a64748
Gabriel Cesarino <61214104+gabrielCesarino@users.noreply.github.com>
2022-12-26 11:14:25 -0300

    add: Header component

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -1,10 +1,11 @@
+import { Header } from '../components/Header';
 import { Text } from '../components/Text';
 import { Container } from './styles';
 
 export function Main() {
 	return (
 		<Container>
-			<Text> Main render</Text>
+			<Header />
 		</Container>
 	)
 }
